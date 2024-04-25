bf5b54e425676d3aa77483525a33e6c870db3c2c
Conor Sheridan <80830322+uzum4ke@users.noreply.github.com>
2024-04-25 15:06:59 +0100

    Basic Untyped Lambda Calculus
    Implemented basic parser for Untyped Lambda Calculus

--- main.hs
+++ main.hs
@@ -1,8 +1,41 @@
-ssq :: Int -> Int -> Int
-ssq x y = x*x + y*y
+data Expr
+    = Lam String Expr
+    | App Expr Expr
+    | Ann Expr Type
+    | Var String
+    deriving (Show, Eq)
 
-len :: [a] -> Integer
-len = foldr (\ x -> (+) 1) 0
+data Type
+    = Arrow Type Type
+    | TypeName String
+    deriving (Show, Eq)
 
-appendL :: [a] -> [a] -> [a]
-appendL (x:xs) (y:ys) = foldr (:) (x:xs) (y:ys)
+data SExp
+    = SSymbol String
+    | SList [SExp]
+    deriving (Show, Eq)
+
+parseExpr :: SExp -> Expr
+parseExpr s = case s of
+    SList [SSymbol "\\", SSymbol x, e, SSymbol ":", t] -> Ann (Lam x (parseExpr e)) (parseType t)
+    SList [e1, e2]                                    -> App (parseExpr e1) (parseExpr e2)
+    SSymbol x                                         -> Var x
+    _                                                 -> error $ "bad syntax in expression: " ++ show s
+
+parseType :: SExp -> Type
+parseType t = case t of
+    SList [SSymbol t1, SSymbol "->", SSymbol t2] -> Arrow (TypeName t1) (TypeName t2)
+    SSymbol x                                    -> TypeName x
+    _                                            -> error $ "bad syntax in type: " ++ show t
+
+main :: IO ()
+main = do
+    let exampleExpr = SList [SSymbol "\\", SSymbol "x", SList [SSymbol "f", SSymbol "x"], SSymbol ":", SList [SSymbol "Int", SSymbol "->", SSymbol "Int"]]
+    let parsedExpr = parseExpr exampleExpr
+    putStrLn "Parsed Expression:"
+    print parsedExpr
+
+    let exampleType = SList [SSymbol "Int", SSymbol "->", SSymbol "Int"]
+    let parsedType = parseType exampleType
+    putStrLn "\nParsed Type:"
+    print parsedType
